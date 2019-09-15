4fcaf3dc70fa5d5f8c80c0623683daa5f0dc607e
Sandy Masia <macxermillio@gmail.com>
2019-09-15 13:23:17 +0200

    "Added more tracks"

--- JS/data.js
+++ JS/data.js
@@ -1,34 +1,118 @@
 // Data entries
 const gamesEntries = [
   {
-    title: "Fifa 20", // Game title (name)
+    title: "Fifa 19", // Game title (name)
     year: "2018", // Year when game was released
-    cover: "cover.jpg", // Example: cover-art.jpg
+    cover: "Fifa.jpg", // Example: cover-art.jpg
     songs: [
       {
-        title: "Another Sky",
-        artist: "The Cracks",
-        album: "Seasons",
-        genre: "Pop"
+        title: "1,000 Opera Singers Working In Starbucks",
+        artist: "Wovoka Gentle",
+        album: "1,000 Opera Singers Working In Starbucks",
+        genre: "Rock",
+        youtube:"fPROPYav1dQ"
       },
       {
-        title: "The Waves",
-        artist: "Bastille",
-        album: "Doom Days",
-        genre: "Indie Rock"
+        title: "Another Level",
+        artist: "Crystal Fighters",
+        album: "Gaia & Friends",
+        genre: "Pop",
+        youtube:"20hZSo0k_gM"
       },
       {
-        title: "Title #1",
-        artist: "Artist #1",
-        album: "Album #1",
-        genre: "metal"
+        title: "Beat 54 (All Good Now)",
+        artist: "Jungle",
+        album: "For Ever",
+        genre: "Disco",
+       youtube :"SetClFl10cQ"
       },
       {
-        title: 'This song title is long',
-        artist: 'This Artist name Here',
-        album: 'This album Title Is Long',
-        genre: 'Unnecessary-long-genre'
-      }
+        title: 'Beautiful People',
+        artist: 'Andreya Triana',
+        album: 'Life in Colour',
+        genre: 'Pop',
+        youtube:"dEsOTfcT3H8"
+      },
+      {
+        title:"Big Dreams",
+        artist:"Bakar",
+        album:"Badkid",
+        genre:"Indie",
+        youtube: "2TasEMbkFHY"
+      },
+      {
+        title:"Blue Light",
+        artist:"SUN SILVA",
+        album:"Silhouettes (Acoustic)",
+        genre:"Pop",
+        youtube:"GWPJs0tur-0"
+      },
+      {
+        title:"Border Girl",
+        artist:"Young fathers",
+        album:"Cocoa Sugar",
+        genre:"Indie",
+        youtube: "6FDE09tjdFU"
+      },
+      {
+        title:"City Looks Pretty",
+        artist:"Courtney Barnett",
+        album:"Tell Me How You Really Feel",
+        genre:"Alternative/Indie",
+        youtube:"42XXZvrnHbM"
+      },
+      {
+        title:"Consistent",
+        artist:"No/Me",
+        album:"Consistent",
+        genre:"Alternative/Indie",
+        youtube:"LD6w8tj0Uyo"
+      },
+      {
+        title:"Drama",
+        artist:"NoMBe (Feat. Big Data)",
+        album:"They Might've Even Loved Me",
+        genre:"Alternative/Indie",
+        youtube:"EJ1YhZ2fUZE"
+      },
+      {
+        title:"",
+        artist:"",
+        album:"",
+        genre:"",
+        youtube:""
+      },
+      {
+        title:"",
+        artist:"",
+        album:"",
+        genre:"",
+        youtube:""
+      },
+      {
+        title:"",
+        artist:"",
+        album:"",
+        genre:"",
+        youtube:""
+      },
+      {
+        title:"",
+        artist:"",
+        album:"",
+        genre:"",
+        youtube:""
+      },
+      {
+        title:"",
+        artist:"",
+        album:"",
+        genre:"",
+        youtube:""
+      },
+      
+     
+
     ]
   },
   {
